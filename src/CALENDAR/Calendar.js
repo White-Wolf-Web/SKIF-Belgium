71514b5cdcf43ad4f8d06ce9bb93ecc5d6f2baec
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2023-04-22 21:38:22 +0200

    mini bug footer

--- src/CALENDAR/Calendar.js
+++ src/CALENDAR/Calendar.js
@@ -6,98 +6,90 @@ import { useTranslation } from "react-i18next";
 import CalendarPdf from "./CalendarPdf";
 import "../i18n";
 import FilterContainer from "./CalendarFilter";
-import Loader from "../UTILS/Loader.js"
+import Loader from "../UTILS/Loader.js";
 
 export default function Calendar(props) {
 	const { t } = useTranslation();
 	const [items, setItems] = useState([]);
-	const [loading, setLoading] = useState(true); 
+	const [loading, setLoading] = useState(true);
 	const urlPdf = "https://skifb-admin.be/api/CalendarAPI/GetFile?filename=";
 
 	useEffect(() => {
 		async function fetchData() {
-		  try {
-			const response = await fetch("https://skifb-admin.be/api/CalendarAPI/GetCalendar");
-			const data = await response.json();
-			setItems(data);
-			setLoading(false); 
-		  } catch (e) {
-			console.log(e);
-			setLoading(false); 
-		  }
+			try {
+				const response = await fetch("https://skifb-admin.be/api/CalendarAPI/GetCalendar");
+				const data = await response.json();
+				setItems(data);
+				setLoading(false);
+			} catch (e) {
+				console.log(e);
+				setLoading(false);
+			}
 		}
 		fetchData();
-	  }, []);
-	
+	}, []);
 
 	return (
 		<main>
 			<h1>{t("federal_calendar.h1")}</h1>
 			<FilterContainer />
 			<div className="calendarReact">
-			{loading ? ( <Loader /> ) : (
-				<div className="calendarAccordion">
-					{items.map((item) => (
-						<Accordion defaultActiveKey="false" key={item.id} flush>
-							<Accordion.Item eventKey="0">
-								<Accordion.Header>
-									<div className="calendar-accordion-header">
-										<div>{" "}
-										{item.startDay +
-											"/" +
-											item.startMonth +
-											"/" +
-											item.startYear}{" "}</div>
-										<div> <span className="breaking-space"></span>  {item.title}</div>
-									</div>
-								</Accordion.Header>
-								<Accordion.Body className="calendarBody">
-									<div className="calendarCardText">
-										<div>
-											{t("federal_calendar.timetable")} {item.startHours}{" "}
-											{t("federal_calendar.to")} {item.endHours + "."}
+				{loading ? (
+					<Loader />
+				) : (
+					<div className="calendarAccordion">
+						{items.map((item) => (
+							<Accordion defaultActiveKey="false" key={item.id} flush>
+								<Accordion.Item eventKey="0">
+									<Accordion.Header>
+										<div className="calendar-accordion-header">
+											<div> {item.startDay + "/" + item.startMonth + "/" + item.startYear} </div>
+											<div>
+												{" "}
+												<span className="breaking-space"></span> {item.title}
+											</div>
 										</div>
-										<div>
-											{t("federal_calendar.location")} {item.club}
-										</div>
-										<div>
-											{t("federal_calendar.adress")} {item.addressStreet}{" "}
-											{item.addressNumber + ","} {item.addressCity}{" "}
-											{item.addressPostalCode}{" "}
-										</div>
-										<div className="br">
-											{"Province:"} {item.addressProvince} &emsp;{" "}
-											{item.addressCountry}
+									</Accordion.Header>
+									<Accordion.Body className="calendarBody">
+										<div className="calendarCardText">
+											<div>
+												{t("federal_calendar.timetable")} {item.startHours} {t("federal_calendar.to")} {item.endHours + "."}
+											</div>
+											<div>
+												{t("federal_calendar.location")} {item.club}
+											</div>
+											<div>
+												{t("federal_calendar.adress")} {item.addressStreet} {item.addressNumber + ","} {item.addressCity} {item.addressPostalCode}{" "}
+											</div>
+											<div className="br">
+												{"Province:"} {item.addressProvince} &emsp; {item.addressCountry}
+											</div>
+
+											<div>
+												{" "}
+												{t("federal_calendar.contact")}
+												{item.contact}
+											</div>
+											<div className="br">
+												{" "}
+												{t("federal_calendar.mail")}
+												{item.email}
+											</div>
+											<div>
+												<div className="br">{item.price}</div>
+											</div>
+
+											<div>{item.description}</div>
 										</div>
 
-										<div>
-											{" "}
-											{t("federal_calendar.contact")}
-											{item.contact}
-										</div>
-										<div className="br">
-											{" "}
-											{t("federal_calendar.mail")}
-											{item.email}
-										</div>
-										<div>
-											<div className="br">{item.price}</div>
-										</div>
-
-										<div>{item.description}</div>
-									</div>
-
-									<CalendarPdf
-										linkPdf={urlPdf + item.linkPdf}
-										coverImage={item.coverImage}
-									/>
-								</Accordion.Body>
-							</Accordion.Item>
+										<CalendarPdf linkPdf={urlPdf + item.linkPdf} coverImage={item.coverImage} />
+									</Accordion.Body>
+								</Accordion.Item>
 							</Accordion>
-            ))}
-          </div>
-        )}
-      </div>
-    </main>
-  );
+						))}
+					</div>
+				)}
+			</div>
+		</main>
+	);
 }
